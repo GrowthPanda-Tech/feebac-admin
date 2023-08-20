20e466468d727c053f93d6576249587c6c17756b
Rahul Choudhury <rahul@growthpanda.co>
2023-08-20 23:37:41 +0530

    refactor(user-table): remove custom hook

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,24 +1,24 @@
-import useStateEffect from '../../hooks/useStateEffect';
+import { useState, useEffect } from 'react';
 import TableHead from '../TableHead';
 import TableData from '../TableData';
-
-//TODO: should i export this to utils?
-function calculateAge(date) {
-    const birthDate = new Date(date);
-    const currentDate = new Date();
-
-    let roundedAge;
-
-    const ageInMiliSec = currentDate - birthDate;
-    const ageInYears = ageInMiliSec / (365 * 24 * 60 * 60 * 1000);
-    const floorValue = Math.floor(ageInYears);
-
-    floorValue === 0 ? roundedAge = '-' : roundedAge = floorValue;
-
-    return roundedAge;
-}
+import makeRequest from '../../utils/makeRequest';
 
 function TableBody({rowData}) {
+    const calculateAge = (date) => {
+        const birthDate = new Date(date);
+        const currentDate = new Date();
+
+        let roundedAge;
+
+        const ageInMiliSec = currentDate - birthDate;
+        const ageInYears = ageInMiliSec / (365 * 24 * 60 * 60 * 1000);
+        const floorValue = Math.floor(ageInYears);
+
+        floorValue === 0 ? roundedAge = '-' : roundedAge = floorValue;
+
+        return roundedAge;
+    }
+
     return (
         <tbody className="text-lg">
             {
@@ -26,10 +26,10 @@ function TableBody({rowData}) {
                     <tr key={index}>
                         <TableData data={index + 1} />
                         <TableData data={data.user_id.split('-').pop()} />
-                        <TableData data={data.gender === null ? '-' : data.gender} />
-                        <TableData data={data.date_of_birth === null ? '-' : calculateAge(data.date_of_birth)} />
+                        <TableData data={data.gender ? data.gender: '-'} />
+                        <TableData data={data.date_of_birth ? calculateAge(data.date_of_birth) : '-'} />
                         <TableData data={data.loyalty_points} />
-                        <TableData data={data.state === null ? '-' : data.state} />
+                        <TableData data={data.state ? data.state : '-'} />
                     </tr>
                 ))
             }
@@ -38,15 +38,25 @@ function TableBody({rowData}) {
 }
 
 export default function User() {
-    const userList = useStateEffect('array', 'site-admin/get-all-user', 'GET');
     const headers = ["No.", "User ID", "Gender", "Age", "Loyalty Points", "Location"];
+
+    const [userData, setUserData] = useState([]);
+
+    const getUserData = async () => {
+        const response = await makeRequest('site-admin/get-all-user', 'GET');
+        response.isSuccess ? setUserData(response.data) : alert(response.message);
+    }
+
+    useEffect(() => {
+        getUserData();
+    }, []);
 
     return (
         <>
             <h1 className="heading"> User Information </h1>
             <table className="table-fixed w-full bg-white rounded-xl text-center">
                 <TableHead headers={headers} />
-                <TableBody rowData={userList} />
+                <TableBody rowData={userData} />
             </table>
         </>
     );

7cdc30fe378c8379995199bb4911785527be45cb
Sabir-growthpanda <sabir@growthpanda.co>
2023-09-06 18:34:05 +0530

    Fix & Added: Article Edit form  Responsive and 404 page

--- src/components/pageNotFound/PageNotFound.jsx
+++ src/components/pageNotFound/PageNotFound.jsx
@@ -2,5 +2,14 @@ import pageNotFoundImage from "../../assets/pageNotFound.png";
 import { Link } from "react-router-dom";
 
 export default function PageNotFound() {
-    return <></>;
+    return <>
+    <div className="flex flex-col justify-center h-auto items-center">
+        <img className="h-auto md:h-[70vh]" src={pageNotFoundImage} alt="page" />
+        <Link to="/" className="btn-primary drop-shadow-md">
+            Go to Dashboard
+        </Link>
+
+    </div>
+  
+    </>;
 }

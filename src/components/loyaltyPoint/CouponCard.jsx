f233fd20e9e88e4728978a180316d8357d16a527
Rahul Choudhury <rchoudhury63@gmail.com>
2023-10-04 17:37:06 +0530

    fix(loyalty): apply disable filter to coupon cards properly
    remove time from coupon cards

--- src/components/loyaltyPoint/CouponCard.jsx
+++ src/components/loyaltyPoint/CouponCard.jsx
@@ -1,56 +1,46 @@
 import CouponToggle from "./CouponToggle";
 
 export default function CouponCard({ data, setCouponsData }) {
-    console.log(data);
     return (
         <>
             <div
-                className={` relative flex   bg-white rounded-xl shadow-xl flex-col  `}
+                className={`relative flex ${
+                    data.isActive ? "" : "disable-filter"
+                } bg-white rounded-xl shadow-xl flex-col`}
             >
                 {!data.isActive && (
-                    <h2 className=" text-2xl text-white z-10 absolute top-[50%]  left-[36%]">
+                    <h2 className="text-2xl text-white z-10 absolute top-[50%] left-[36%]">
                         Disabled
                     </h2>
                 )}
 
-                <div
-                    className={`bg rounded-lg ${
-                        data.isActive ? "" : "disable-filter"
-                    } `}
-                >
+                <div className="rounded-lg">
                     <div className="absolute top-1 right-2 p-2">
                         <CouponToggle
                             couponId={data.id}
                             couponInfo={data}
                             setCouponsData={setCouponsData}
                         />
-                        {/* <i className="fa-solid fa-ellipsis-vertical"></i> */}
                     </div>
 
-                    <div className=" card-img flex items-center justify-center">
+                    <div className="card-img flex items-center justify-center">
                         <img className=" w-52" src={data.imageUrl} alt="" />
                     </div>
 
-                    <div className="pb-10">
-                        <h2 className="text-black pl-6 text-2xl font-semibold">
+                    <div className="py-10 px-6 flex flex-col gap-4">
+                        <h2 className="text-black text-2xl font-semibold">
                             {data.title}
                         </h2>
-                        <div className="flex pl-6 flex-grow flex-col gap-2 font-medium">
+                        <div className="flex flex-grow flex-col gap-1 font-medium">
                             <h2>{data.description}</h2>
-                            <span className="gap-2">
-                                <i className="  pr-2 fa-regular fa-clock"></i>
-                                valid till {data.expiredData}
+                            <span className="flex items-center gap-2">
+                                <i className="fa-regular fa-clock"></i>
+                                <span>{data.expiredData.split(" ")[0]}</span>
                             </span>
-                            <div className=" flex bg-[#5927E8] w-24 rounded-full items-center justify-evenly text-white ">
-                                <i className="fa-solid fa-coins p-2"></i>
-                                <h2 className=" p-2">{data.value}</h2>
-                            </div>
-                            {/* {data.details.map((item, index) => {
-                                return <span key={index}>{item}</span>;
-                            })}
-                            {data.tnc.map((item, index) => {
-                                return <span key={index}>{item}</span>;
-                            })} */}
+                        </div>
+                        <div className="flex bg-[#5927E8] w-24 p-2 rounded-full items-center justify-evenly text-white ">
+                            <i className="fa-solid fa-coins"></i>
+                            <h2>{data.value}</h2>
                         </div>
                     </div>
                 </div>

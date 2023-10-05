c745f23eb9bdb6b452a77a4676cc784481471b15
Rahul Choudhury <rchoudhury63@gmail.com>
2023-10-05 15:50:02 +0530

    feat(utils): add restrict function for number type input

---
+++ src/utils/removeForbiddenChars.js
@@ -0,0 +1,20 @@
+export default function removeForbiddenChars(event) {
+    const type = event.type;
+    const key = event.key;
+
+    const forbiddenChars = ["e", "E", "+", "-", ".", " "];
+
+    if (type === "keydown") {
+        if (forbiddenChars.includes(key)) {
+            event.preventDefault();
+        }
+
+        return;
+    }
+
+    if (type === "paste") {
+        event.preventDefault();
+
+        return;
+    }
+}

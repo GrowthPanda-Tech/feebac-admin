b74c7d181a24975ac8e1bae70ca401430a18337e
Rahul Choudhury <rahul@growthpanda.co>
2023-10-10 11:32:38 +0530

    feat(survey): add loading state to survey table

--- src/components/survey/Survey.jsx
+++ src/components/survey/Survey.jsx
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
+import { Spinner } from "@material-tailwind/react";
+
 import Table from "../table/Table";
 import Thead from "../table/Thead";
 import Trow from "../table/Trow";
 import Tdata from "../table/Tdata";
 import PageTitle from "../PageTitle";
-import makeRequest from "../../utils/makeRequest";
 
-//assets
-import edit from "../../assets/edit.svg";
+import makeRequest from "../../utils/makeRequest";
 
 const HEADERS = ["Title", "Category", "Start Date", "End Date", "Actions"];
 
@@ -62,10 +62,11 @@ function ButtonComponent({ setStatus }) {
 export default function Survey() {
     const [surveyData, setsurveyData] = useState([]);
     const [status, setStatus] = useState("live");
+    const [loading, setLoading] = useState(false);
 
     const convertToLocal = (date) => {
         const dateObj = new Date(`${date} UTC`);
-        return dateObj.toLocaleString();
+        return dateObj.toLocaleString("en-GB", { hour12: true });
     };
 
     useEffect(() => {
@@ -73,6 +74,8 @@ export default function Survey() {
 
         async function fetchSurveyData() {
             try {
+                setLoading(true);
+
                 const response = await makeRequest(
                     `site-admin/get-all-survey?time=${status}`
                 );
@@ -83,6 +86,7 @@ export default function Survey() {
 
                 if (!ignore) {
                     setsurveyData(response.data);
+                    setLoading(false);
                 }
             } catch (error) {
                 console.error(error);
@@ -132,101 +136,111 @@ export default function Survey() {
 
             <ButtonComponent setStatus={setStatus} />
 
-            <div className=" h-[60vh] bg-white overflow-y-scroll">
-                <Table>
-                    <Thead headers={HEADERS} />
-                    <tbody>
-                        {surveyData
-                            .toReversed()
-                            .map(
-                                ({
-                                    survey_id,
-                                    survey_title,
-                                    category,
-                                    start_date,
-                                    end_date,
-                                }) => (
-                                    <Trow key={survey_id}>
-                                        <Tdata left> {survey_title} </Tdata>
-                                        <Tdata capitalize> {category} </Tdata>
-                                        <Tdata mono>
-                                            <div className="flex flex-col gap-2">
-                                                <div>
-                                                    {
-                                                        convertToLocal(
-                                                            start_date
-                                                        ).split(",")[0]
-                                                    }
-                                                </div>
-                                                <div className="text-sm">
-                                                    {
-                                                        convertToLocal(
-                                                            start_date
-                                                        ).split(",")[1]
-                                                    }
-                                                </div>
-                                            </div>
-                                        </Tdata>
-                                        <Tdata mono>
-                                            <div className="flex flex-col gap-2">
-                                                <div>
-                                                    {
-                                                        convertToLocal(
-                                                            end_date
-                                                        ).split(",")[0]
-                                                    }
-                                                </div>
-                                                <div className="text-sm ">
-                                                    {
-                                                        convertToLocal(
-                                                            end_date
-                                                        ).split(",")[1]
-                                                    }
-                                                </div>
-                                            </div>
-                                        </Tdata>
-                                        <Tdata>
-                                            <div className="flex justify-center gap-4">
-                                                {status === "live" ||
-                                                status === "expired" ? (
-                                                    <div className="flex justify-center">
-                                                        <div className="tool-tip-div group">
-                                                            <Link
-                                                                to={`details/${survey_id}`}
-                                                            >
-                                                                <i className="fa-solid fa-square-poll-horizontal text-xl"></i>
-                                                            </Link>
-                                                            <span className="tool-tip-span -right-[3.4rem] bg-black -top-12 ">
-                                                                View Response
-                                                                <span className="tooltip-arrow bottom-[-2px] left-[45%]"></span>
-                                                            </span>
-                                                        </div>
+            <div className="h-[60vh] bg-white overflow-y-scroll">
+                {loading ? (
+                    <div className="flex h-full w-full justify-center items-center">
+                        <Spinner className="h-12 w-12" />
+                    </div>
+                ) : (
+                    <Table>
+                        <Thead headers={HEADERS} />
+                        <tbody>
+                            {surveyData
+                                .toReversed()
+                                .map(
+                                    ({
+                                        survey_id,
+                                        survey_title,
+                                        category,
+                                        start_date,
+                                        end_date,
+                                    }) => (
+                                        <Trow key={survey_id}>
+                                            <Tdata left> {survey_title} </Tdata>
+                                            <Tdata capitalize>
+                                                {" "}
+                                                {category}{" "}
+                                            </Tdata>
+                                            <Tdata mono>
+                                                <div className="flex flex-col gap-2">
+                                                    <div>
+                                                        {
+                                                            convertToLocal(
+                                                                start_date
+                                                            ).split(",")[0]
+                                                        }
                                                     </div>
-                                                ) : (
-                                                    ""
-                                                )}
-                                                {status === "upcoming" ? (
-                                                    <div className="flex justify-center">
-                                                        <div className="tool-tip-div group">
-                                                            <Link
-                                                                to={`edit-survey/${survey_id}`}
-                                                            >
-                                                                <i className="fa-solid fa-pen-to-square text-xl"></i>
-                                                            </Link>
-                                                            <span className="tool-tip-span -right-[2.8rem] bg-black -top-12 ">
-                                                                Edit Survey
-                                                                <span className="tooltip-arrow bottom-[-2px] left-[38%]"></span>
-                                                            </span>
-                                                        </div>
+                                                    <div className="text-sm">
+                                                        {
+                                                            convertToLocal(
+                                                                start_date
+                                                            ).split(",")[1]
+                                                        }
                                                     </div>
-                                                ) : null}
-                                            </div>
-                                        </Tdata>
-                                    </Trow>
-                                )
-                            )}
-                    </tbody>
-                </Table>
+                                                </div>
+                                            </Tdata>
+                                            <Tdata mono>
+                                                <div className="flex flex-col gap-2">
+                                                    <div>
+                                                        {
+                                                            convertToLocal(
+                                                                end_date
+                                                            ).split(",")[0]
+                                                        }
+                                                    </div>
+                                                    <div className="text-sm ">
+                                                        {
+                                                            convertToLocal(
+                                                                end_date
+                                                            ).split(",")[1]
+                                                        }
+                                                    </div>
+                                                </div>
+                                            </Tdata>
+                                            <Tdata>
+                                                <div className="flex justify-center gap-4">
+                                                    {status === "live" ||
+                                                    status === "expired" ? (
+                                                        <div className="flex justify-center">
+                                                            <div className="tool-tip-div group">
+                                                                <Link
+                                                                    to={`details/${survey_id}`}
+                                                                >
+                                                                    <i className="fa-solid fa-square-poll-horizontal text-xl"></i>
+                                                                </Link>
+                                                                <span className="tool-tip-span -right-[3.4rem] bg-black -top-12 ">
+                                                                    View
+                                                                    Response
+                                                                    <span className="tooltip-arrow bottom-[-2px] left-[45%]"></span>
+                                                                </span>
+                                                            </div>
+                                                        </div>
+                                                    ) : (
+                                                        ""
+                                                    )}
+                                                    {status === "upcoming" ? (
+                                                        <div className="flex justify-center">
+                                                            <div className="tool-tip-div group">
+                                                                <Link
+                                                                    to={`edit-survey/${survey_id}`}
+                                                                >
+                                                                    <i className="fa-solid fa-pen-to-square text-xl"></i>
+                                                                </Link>
+                                                                <span className="tool-tip-span -right-[2.8rem] bg-black -top-12 ">
+                                                                    Edit Survey
+                                                                    <span className="tooltip-arrow bottom-[-2px] left-[38%]"></span>
+                                                                </span>
+                                                            </div>
+                                                        </div>
+                                                    ) : null}
+                                                </div>
+                                            </Tdata>
+                                        </Trow>
+                                    )
+                                )}
+                        </tbody>
+                    </Table>
+                )}
             </div>
         </div>
     );

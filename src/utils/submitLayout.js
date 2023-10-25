679789a077d5988b5e8522d4b83a2365c371c746
Rahul Choudhury <rahul@growthpanda.co>
2023-10-25 15:04:51 +0530

    feat(insights): add a layout submit util function
    TODO: make it a bit more dynamic

---
+++ src/utils/submitLayout.js
@@ -0,0 +1,32 @@
+import makeRequest from "./makeRequest";
+
+export default async function submitLayout(layout, index, pages, setPages) {
+    let route = "insights/add-insights-pages";
+    let method = "POST";
+
+    if (layout.id) {
+        route = "insights/update-insight-pages";
+        method = "PUT";
+    }
+
+    try {
+        const response = await makeRequest(route, method, layout);
+
+        if (!response.isSuccess) {
+            throw new Error(response.message);
+        }
+
+        if (layout.id) {
+            const updatedPages = [...pages];
+            updatedPages[index] = layout;
+
+            setPages(updatedPages);
+        } else {
+            const updatedPages = [...pages, { ...layout, id: response.data }];
+
+            setPages(updatedPages);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}

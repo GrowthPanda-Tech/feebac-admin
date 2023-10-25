e69b8c52d6dab34db5e261fd63867442ad0968dc
Rahul Choudhury <rahul@growthpanda.co>
2023-10-25 14:59:49 +0530

    feat(insights): create context for insight pages
    wrap App.jsx with said context
    add reducer to context
    RIP redux 🙂

---
+++ src/contexts/InsightPageContext.jsx
@@ -0,0 +1,61 @@
+//who needs redux
+
+import { createContext, useState, useEffect, useReducer } from "react";
+
+const INIT_STATE = {
+    pageType: 1,
+    data: null,
+    index: null,
+};
+
+function insightReducer(state, action) {
+    switch (action.type) {
+        case "LAYOUT": {
+            return {
+                pageType: action.pageType,
+                data: null,
+                index: null,
+            };
+        }
+
+        case "PAGE": {
+            return {
+                ...state,
+                pageType: action.pageType,
+                data: action.data,
+                index: action.index,
+            };
+        }
+
+        case "RESET": {
+            return {
+                ...INIT_STATE,
+            };
+        }
+
+        default: {
+            throw new Error(`Unknown action: ${action.type}`);
+        }
+    }
+}
+
+export const PageContext = createContext(null);
+
+export default function PageContextProvider({ children }) {
+    const pagesSession = JSON.parse(sessionStorage.getItem("insightPages"));
+
+    const [pages, setPages] = useState(pagesSession ? pagesSession : []);
+    const [state, dispatch] = useReducer(insightReducer, INIT_STATE);
+
+    useEffect(() => {
+        if (pages.length > 0) {
+            sessionStorage.setItem("insightPages", JSON.stringify(pages));
+        }
+    }, [pages]);
+
+    return (
+        <PageContext.Provider value={{ pages, setPages, state, dispatch }}>
+            {children}
+        </PageContext.Provider>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -8,16 +8,19 @@ import { ThemeProvider } from "@material-tailwind/react";
 
 import CategoryContextProvider from "./contexts/CategoryContext.jsx";
 import ProfileContextProvider from "./contexts/ProfileContext.jsx";
+import PageContextProvider from "./contexts/InsightPageContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <BrowserRouter>
             <ThemeProvider>
-                <ProfileContextProvider>
-                    <CategoryContextProvider>
-                        <App />
-                    </CategoryContextProvider>
-                </ProfileContextProvider>
+                <PageContextProvider>
+                    <ProfileContextProvider>
+                        <CategoryContextProvider>
+                            <App />
+                        </CategoryContextProvider>
+                    </ProfileContextProvider>
+                </PageContextProvider>
             </ThemeProvider>
         </BrowserRouter>
     </React.StrictMode>

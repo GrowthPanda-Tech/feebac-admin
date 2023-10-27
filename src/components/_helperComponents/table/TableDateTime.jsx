5d324a89e4693d58e1fdf322ddcec9104053731b
Rahul Choudhury <rahul@growthpanda.co>
2023-10-27 11:43:05 +0530

    feat: add status chip & table helper components

---
+++ src/components/_helperComponents/table/TableDateTime.jsx
@@ -0,0 +1,14 @@
+export default function TableDateTime({ date }) {
+    const dateObj = new Date(date);
+    const localeString = dateObj.toLocaleString("en-GB");
+
+    const dateLocale = localeString.split(",")[0];
+    const timeLocale = localeString.split(",")[1];
+
+    return (
+        <div className="flex flex-col gap-2">
+            <span> {dateLocale} </span>
+            <span className="text-sm"> {timeLocale} </span>
+        </div>
+    );
+}

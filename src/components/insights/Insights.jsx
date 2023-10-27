f76938395b9be848dd632968ec5306ba87168324
Rahul Choudhury <rahul@growthpanda.co>
2023-10-27 11:48:28 +0530

    feat(insights): add table util component
    conditionally render table, insight form & layouts

--- src/components/insights/Insights.jsx
+++ src/components/insights/Insights.jsx
@@ -2,17 +2,21 @@ import { useState, useContext } from "react";
 import { PageContext } from "../../contexts/InsightPageContext";
 
 import makeRequest from "../../utils/makeRequest";
+import swal from "../../utils/swal";
 
 //components
 import PageTitle from "../_helperComponents/PageTitle";
 import InsightCreate from "./utilComponents/InsightCreate";
 import InsightPageCreate from "./utilComponents/InsightPageCreate";
+import InsightTable from "./utilComponents/InsightTable";
+import PrimaryButton from "../PrimaryButton";
 
 export default function Insights() {
     const { setPages, dispatch } = useContext(PageContext);
     const [insightId, setInsightId] = useState(
         sessionStorage.getItem("insightId")
     );
+    const [isTable, setIsTable] = useState(true);
 
     const handleSubmit = async () => {
         try {
@@ -31,16 +35,35 @@ export default function Insights() {
 
             setInsightId(null);
             setPages([]);
+
+            setIsTable(true);
+
+            swal("success", response.message);
         } catch (error) {
-            console.error(error);
+            swal("error", error.message);
         }
     };
 
     return (
         <div className="flex flex-col gap-8">
-            <PageTitle name={"Insights"} />
+            <div className="flex justify-between items-center">
+                <PageTitle name={"Insights"} />
+                {isTable ? (
+                    <button
+                        className="btn-primary"
+                        onClick={() => setIsTable(false)}
+                    >
+                        Create
+                    </button>
+                ) : null}
+            </div>
 
-            {!insightId ? (
+            {isTable ? (
+                <InsightTable
+                    setInsightId={setInsightId}
+                    setIsTable={setIsTable}
+                />
+            ) : !insightId && !isTable ? (
                 <InsightCreate setInsightId={setInsightId} />
             ) : (
                 <>

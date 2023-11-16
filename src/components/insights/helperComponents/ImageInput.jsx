e1dc9b01f442a2c4eb1fca56f7750cae7c9a214b
Rahul Choudhury <rahul@growthpanda.co>
2023-11-16 12:56:30 +0530

    refactor(insights): image input component

--- src/components/insights/helperComponents/ImageInput.jsx
+++ src/components/insights/helperComponents/ImageInput.jsx
@@ -1,30 +1,31 @@
-import { useState, useRef } from "react";
+import { useContext, useRef } from "react";
+import { InsightContext } from "../../../contexts/InsightContext";
 
-export default function ImageInput({ handleChange }) {
-    const [imgName, setImgName] = useState(null);
+export default function ImageInput({ label, name }) {
+    const { insightModel, setInsightModel } = useContext(InsightContext);
     const imgRef = useRef(null);
 
-    const handleImg = (e) => {
-        handleChange(e);
-
-        const fileName = e.target.files[0].name;
-        setImgName(fileName);
+    const handleChange = (e) => {
+        const file = e.target.files[0];
+        setInsightModel({ ...insightModel, image: file });
     };
 
     return (
         <div className="flex gap-4 flex-col">
-            <span className="capitalize font-semibold">Background Image</span>
+            <span className="capitalize font-semibold">{label}</span>
             <input
-                name="insightsImage"
+                name={name}
                 type="file"
                 accept="image/*"
                 ref={imgRef}
-                onChange={handleImg}
+                onChange={(e) => handleChange(e)}
                 hidden
             />
 
             <div className="bg-white border border-[#1D1D1D] rounded-xl py-5 px-10 flex justify-between">
-                <span>{imgName ? imgName : " "}</span>
+                <span>
+                    {insightModel.image ? insightModel.image.name : " "}
+                </span>
                 <span
                     className="text-secondary cursor-pointer hover:text-primary font-semibold"
                     onClick={() => imgRef.current.click()}

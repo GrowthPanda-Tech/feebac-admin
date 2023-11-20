3c56d91cbf8ac7d686fa5955f726ad9ab43ea0c1
Rahul Choudhury <rahul@growthpanda.co>
2023-11-20 23:05:56 +0530

    refactor(insights): create init state for context

--- src/contexts/InsightContext.jsx
+++ src/contexts/InsightContext.jsx
@@ -1,13 +1,14 @@
 import { useState, createContext } from "react";
 
 export const InsightContext = createContext(null);
+export const INIT_STATE = {
+    caption: "",
+    image: null,
+    pages: [],
+};
 
 export default function InsightContextProvider({ children }) {
-    const [insightModel, setInsightModel] = useState({
-        image: null,
-        pages: [],
-    });
-
+    const [insightModel, setInsightModel] = useState(INIT_STATE);
     return (
         <InsightContext.Provider value={{ insightModel, setInsightModel }}>
             {children}

036e409943085675ade3f182966b0898981e03bc
Rahul Choudhury <rahul@growthpanda.co>
2023-11-20 23:12:03 +0530

    feat: manage insights

--- src/components/insights/Insights.jsx
+++ src/components/insights/Insights.jsx
@@ -1,3 +1,18 @@
+import { Link } from "react-router-dom";
+import PageTitle from "../PageTitle";
+import InsightGrid from "./utilComponents/InsightGrid";
+
 export default function Insights() {
-    return <h1>Insights</h1>;
+    return (
+        <div className="flex flex-col gap-6">
+            <div className="flex justify-between items-center">
+                <PageTitle name={"Insights"} />
+                <Link className="btn-primary" to={"create"}>
+                    Create Insight
+                </Link>
+            </div>
+
+            <InsightGrid />
+        </div>
+    );
 }

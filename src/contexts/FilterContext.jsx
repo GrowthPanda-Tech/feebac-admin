12ee8826821d4d34d6cba86bd217d55843259d52
Rahul Choudhury <rahul@growthpanda.co>
2023-11-20 23:19:24 +0530

    fix: change var names in filter context

--- src/contexts/FilterContext.jsx
+++ src/contexts/FilterContext.jsx
@@ -5,12 +5,12 @@ import useFetch from "../hooks/useFetch";
 export const FilterContext = createContext(null);
 
 export default function FilterContextProvider({ children }) {
-    const { loading, response, setResponse, error } = useFetch(
+    const { loading, fetchedData, setFetchedData, error } = useFetch(
         "config/get-profile-key-value"
     );
 
     return (
-        <FilterContext.Provider value={{ response, setResponse }}>
+        <FilterContext.Provider value={{ fetchedData, setFetchedData }}>
             {children}
         </FilterContext.Provider>
     );

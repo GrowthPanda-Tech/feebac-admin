d6158fb01709441584d5bdf1d1c23bbf1488862d
Rahul Choudhury <rahul@growthpanda.co>
2023-12-06 12:05:46 +0530

    fix: coupon category selector

--- src/components/loyaltyPoint/CouponCategory.jsx
+++ src/components/loyaltyPoint/CouponCategory.jsx
@@ -8,20 +8,26 @@ export default function CouponCategory({
   const [selectedValue, setSelectedValue] = useState(selectedValueProp);
 
   const handleChange = (event) => {
-    setSelectedValue(event.target.value);
+    const valueNum = parseInt(event.target.value);
+
+    setSelectedValue(valueNum);
     setAddCouponData((prev) => ({
       ...prev,
-      category: event.target.value,
+      category: valueNum,
     }));
   };
+
   useEffect(() => {
     const selectedOption = options.find(
       (option) => option.name === selectedValueProp
     );
+
     let selectedOptions = selectedOption
       ? selectedOption.id
       : selectedValueProp;
+
     setSelectedValue(selectedOptions);
+
     if (selectedOption !== undefined) {
       setAddCouponData((prev) => ({
         ...prev,
@@ -36,14 +42,14 @@ export default function CouponCategory({
     <div className="mb-2 flex items-center gap-4">
       <label className="font-semibold ">Select Category for Coupon</label>
       <select
-        className="px-2 py-2 ml-2 border input-article w-40  rounded-md"
+        className="px-2 py-2 ml-2 border input-article w-40 rounded-md capitalize"
         value={selectedValue}
         onChange={handleChange}
         required
       >
         <option value="">Select option</option>
         {options.map((option) => (
-          <option key={option.id} value={option.id} className="capitalize">
+          <option key={option.category_id} value={option.category_id}>
             {option.category_name}
           </option>
         ))}

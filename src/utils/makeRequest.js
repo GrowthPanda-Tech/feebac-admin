399d5f4667aa5b43caf40c719c9a59d29d969d58
Rahul Choudhury <rahul@growthpanda.co>
2023-12-08 11:31:19 +0530

    refactor: migrate to Authorization header in network calls

--- src/utils/makeRequest.js
+++ src/utils/makeRequest.js
@@ -1,32 +1,29 @@
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 const AUTH_TOKEN = localStorage.getItem("authToken");
 
+//TODO: implement timeout
 export default async function makeRequest(route, method = "GET", body = null) {
   const request = {
     method,
     headers: {
-      authToken: AUTH_TOKEN,
       Accept: "application/json",
-      // Authorization: `Bearer ${AUTH_TOKEN}`,
+      Authorization: `Bearer ${AUTH_TOKEN}`,
     },
   };
 
-  if (body) {
+  if (body)
     request.body = body instanceof FormData ? body : JSON.stringify(body);
-  }
 
   try {
     const response = await fetch(`${BASE_URL}/${route}`, request);
     const statusCode = response.status;
 
-    //TODO: error based on respnse.ok
-    if (statusCode >= 500 || statusCode === 204) {
+    if (!response.ok || statusCode >= 500 || statusCode === 204)
       throw new Error(statusCode);
-    }
 
     return await response.json();
   } catch (error) {
-    //TODO: throw different error based on status code
+    console.error(error);
     throw error;
   }
 }

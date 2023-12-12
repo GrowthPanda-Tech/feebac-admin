af6f23c3afb208eef17734645e0e24ee3725fcd3
Rahul Choudhury <rahul@growthpanda.co>
2023-12-12 13:30:16 +0530

    feat: add jsdoc spec for swal util

--- src/utils/swal.js
+++ src/utils/swal.js
@@ -3,6 +3,12 @@ import withReactContent from "sweetalert2-react-content";
 
 const MySwal = withReactContent(Swal);
 
+/**
+ * Displays a toast notification using SweetAlert2 with React integration.
+ *
+ * @param {"success" | "error" | "warning" | "info" | "question"} type - The type of the toast notification.
+ * @param {string} message - The message to be displayed in the toast notification.
+ */
 export default function swal(type, message) {
   const title = type.charAt(0).toUpperCase() + type.substring(1);
 

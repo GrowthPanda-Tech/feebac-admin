ff0aa3bc0183b0ac357f0deb9f4c3a72f1f9d860
Rahul Choudhury <rahul@growthpanda.co>
2023-12-29 16:35:02 +0530

    feat: add delete support for three-dot-menu
    add hover background to menu button
    decrease opacity while loading

--- src/components/__helperComponents__/MenuButton.jsx
+++ src/components/__helperComponents__/MenuButton.jsx
@@ -1,8 +1,9 @@
-export default function MenuButton({ name, handler }) {
+export default function MenuButton({ name, handler, loading }) {
   return (
     <button
-      className="cursor-pointer px-5 py-4 text-left transition hover:bg-light-grey"
+      className="cursor-pointer px-5 py-4 text-left transition hover:bg-light-grey disabled:cursor-not-allowed disabled:opacity-50"
       onClick={handler}
+      disabled={loading}
     >
       {name}
     </button>

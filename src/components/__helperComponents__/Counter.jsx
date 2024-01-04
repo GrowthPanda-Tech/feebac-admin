2711cc506e79fdfed824e037dc621ddf1d3e0ce6
Rahul Choudhury <rahul@growthpanda.co>
2024-01-04 16:30:05 +0530

    feat: add counter component to display word or characters

---
+++ src/components/__helperComponents__/Counter.jsx
@@ -0,0 +1,10 @@
+export default function Counter({ count, max, type }) {
+  return (
+    <div className="flex gap-1 font-semibold text-dark-grey">
+      <span className={`${count >= max ? `font-bold text-red-500` : ""}`}>
+        {count}
+      </span>
+      /<span>{max}</span>
+    </div>
+  );
+}

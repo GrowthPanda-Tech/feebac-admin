acd8ba39356343f37bc66e3f70389aa33788692a
Rahul Choudhury <rahul@growthpanda.co>
2024-01-30 16:40:01 +0530

    test: manual build chunking

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,17 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("chart.js")) return "chartjs";
+          if (id.includes("sweetalert2")) return "sweetalert2";
+          if (id.includes("node_modules")) return "vendor";
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src/", import.meta.url)),

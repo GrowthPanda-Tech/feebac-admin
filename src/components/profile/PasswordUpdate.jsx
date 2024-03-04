bb07866b08affa996665ba18202c716fb42a3147
Rahul Choudhury <rahul@growthpanda.co>
2024-03-04 16:37:20 +0530

    refactor: states and event handlers in `PasswordUpdate`

--- src/components/profile/PasswordUpdate.jsx
+++ src/components/profile/PasswordUpdate.jsx
@@ -1,55 +1,38 @@
 import { useState } from "react";
-import eye_open from "@/assets/outline_eye_open.png";
-import eye_closed from "@/assets/outline_eye_closed.png";
-import makeRequest from "@/utils/makeRequest";
-import swal from "@/utils/swal";
 import { useNavigate } from "react-router-dom";
 
+// utils
+import makeRequest from "@/utils/makeRequest";
+import swal from "@/utils/swal";
+
+// assets
+import eye_open from "@/assets/outline_eye_open.png";
+import eye_closed from "@/assets/outline_eye_closed.png";
+
 export default function PasswordUpdate() {
-  const [oldPassword, setOldPassword] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-  const [isShowOldPassword, setIsShowOldPassword] = useState(false);
-  const [isShowNewPassword, setIsShowNewPassword] = useState(false);
+  const [password, setPassword] = useState({ old: "", new: "" });
+  const [visibility, setVisibility] = useState({ old: false, new: false });
   const [loading, setLoading] = useState(false);
+
   const navigate = useNavigate();
 
-  const handleOldPasswordChange = (event) => {
-    setOldPassword(event.target.value);
-  };
-
-  const handleNewPasswordChange = (event) => {
-    setNewPassword(event.target.value);
-  };
-
-  const toggleShowOldPassword = () => {
-    setIsShowOldPassword(!isShowOldPassword);
-  };
-
-  const toggleShowNewPassword = () => {
-    setIsShowNewPassword(!isShowNewPassword);
-  };
-
   const handleSubmit = async () => {
-    console.log("Old Password:", oldPassword);
-    console.log("New Password:", newPassword);
     setLoading(true);
+
     try {
       const response = await makeRequest(
         "site-admin/auth/update-password",
         "PATCH",
         {
-          old_password: oldPassword,
-          new_password: newPassword,
+          old_password: password.old,
+          new_password: password.new,
         },
       );
-      console.log(response);
-      if (!response.isSuccess) {
-        throw new Error(response.message);
-      }
+
+      if (!response.isSuccess) throw new Error(response.message);
       swal("success", response.message);
       navigate("/");
     } catch (error) {
-      console.log(error);
       swal("error", error.message);
     } finally {
       setLoading(false);
@@ -62,38 +45,46 @@ export default function PasswordUpdate() {
         <h2 className="mb-6 text-xl font-semibold ">Change Password</h2>
         <div className="relative mb-4">
           <input
-            type={isShowOldPassword ? "text" : "password"}
+            type={visibility.old ? "text" : "password"}
             placeholder="Old Password"
-            className="w-full rounded-lg border border-[#A6ACBE] px-4 py-2 disabled:cursor-not-allowed disabled:bg-white disabled:opacity-50"
-            value={oldPassword}
-            onChange={handleOldPasswordChange}
+            className="w-full rounded-lg border border-[#A6ACBE] px-7 py-4 disabled:cursor-not-allowed disabled:bg-white disabled:opacity-50"
+            value={password.old}
+            onChange={(e) =>
+              setPassword((prev) => ({ ...prev, old: e.target.value }))
+            }
           />
           <img
-            src={!isShowOldPassword ? eye_open : eye_closed}
+            src={!visibility.old ? eye_open : eye_closed}
             className="absolute right-6 top-1/2 w-8 -translate-y-1/2 transform cursor-pointer"
             alt="Toggle Old Password Visibility"
-            onClick={toggleShowOldPassword}
+            onClick={() =>
+              setVisibility((prev) => ({ ...prev, old: !prev.old }))
+            }
           />
         </div>
         <div className="relative mb-4">
           <input
-            type={isShowNewPassword ? "text" : "password"}
+            type={visibility.new ? "text" : "password"}
             placeholder="New Password"
-            className="w-full rounded-lg border border-[#A6ACBE] px-4 py-2 disabled:cursor-not-allowed disabled:bg-white disabled:opacity-50"
-            value={newPassword}
-            onChange={handleNewPasswordChange}
+            className="w-full rounded-lg border border-[#A6ACBE] px-7 py-4 disabled:cursor-not-allowed disabled:bg-white disabled:opacity-50"
+            value={password.new}
+            onChange={(e) =>
+              setPassword((prev) => ({ ...prev, new: e.target.value }))
+            }
           />
           <img
-            src={!isShowNewPassword ? eye_open : eye_closed}
+            src={!visibility.new ? eye_open : eye_closed}
             className="absolute right-6 top-1/2 w-8 -translate-y-1/2 transform cursor-pointer"
             alt="Toggle New Password Visibility"
-            onClick={toggleShowNewPassword}
+            onClick={() =>
+              setVisibility((prev) => ({ ...prev, new: !prev.new }))
+            }
           />
         </div>
         <div className="flex justify-center align-middle">
           <button
             type="submit"
-            className="w-100 text-md mt-5 rounded-lg  bg-secondary px-8 py-4 text-center text-white transition hover:bg-primary disabled:bg-light-grey disabled:text-grey"
+            className="text-md rounded-xl bg-secondary px-11 py-3 text-center font-semibold text-white transition hover:bg-primary disabled:bg-light-grey disabled:text-grey"
             disabled={loading}
             onClick={handleSubmit}
           >
